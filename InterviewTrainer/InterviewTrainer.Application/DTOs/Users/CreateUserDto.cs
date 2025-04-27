a4a2f8e8fb842c1eb2a46c8854ef548e5614c19e
proninp <proninpaul@yandex.ru>
2025-04-27 15:56:02 +0300

    Added user service contract

---
+++ InterviewTrainer/InterviewTrainer.Application/DTOs/Users/CreateUserDto.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewTrainer.Application.DTOs.Users;
+
+public record CreateUserDto
+{
+    public long TelegramId { get; init; }
+    
+    public string? UserName { get; init; }
+    
+    public string? Email {get; init; }
+    
+    public List<Guid> RoleIds { get; init; } = [];
+
+    public CreateUserDto(long telegramId, string? userName = null, string? email = null)
+    {
+        TelegramId = telegramId;
+        UserName = userName;
+        Email = email;
+    }
+}
---
+++ InterviewTrainer/InterviewTrainer.Application/DTOs/Users/UserDto.cs
@@ -0,0 +1,3 @@
+﻿namespace InterviewTrainer.Application.DTOs.Users;
+
+public record UserDto(Guid Id, long TelegramId, string? UserName, string? Email, List<Guid> RoleIds);
