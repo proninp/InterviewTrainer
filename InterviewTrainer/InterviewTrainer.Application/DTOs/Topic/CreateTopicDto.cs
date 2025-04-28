6bba797b46815dfe0b26d7712a2eea2e6c877a41
proninp <proninpaul@yandex.ru>
2025-04-28 10:56:03 +0300

    Added Topic DTOs and Topic service contract

---
+++ InterviewTrainer/InterviewTrainer.Application/DTOs/Topic/CreateTopicDto.cs
@@ -0,0 +1,16 @@
+﻿namespace InterviewTrainer.Application.DTOs.Topic;
+
+public record CreateTopicDto
+{
+    public string Name { get; init; }
+    
+    public bool Archived { get; init; }
+
+    public List<Guid> TechnologyIds { get; init; } = [];
+
+    public CreateTopicDto(Guid id, string name, bool archived)
+    {
+        Name = name;
+        Archived = archived;
+    }
+}
---
+++ InterviewTrainer/InterviewTrainer.Application/DTOs/Topic/TopicDto.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewTrainer.Application.DTOs.Topic;
+
+public record TopicDto
+{
+    public Guid Id { get; init; }
+    
+    public string Name { get; init; }
+    
+    public bool Archived { get; init; }
+
+    public List<Guid> TechnologyIds { get; init; } = [];
+
+    public TopicDto(Guid id, string name, bool archived)
+    {
+        Id = id;
+        Name = name;
+        Archived = archived;
+    }
+}
