3ed094056f44a90c55b1fb069530ce5f82517afd
proninp <proninpaul@yandex.ru>
2025-04-29 10:50:02 +0300

    Added Tag service contract

---
+++ InterviewTrainer/InterviewTrainer.Application/Contracts/Services/ITagService.cs
@@ -0,0 +1,19 @@
+﻿using InterviewTrainer.Application.DTOs.Questions;
+using InterviewTrainer.Application.DTOs.Tags;
+
+namespace InterviewTrainer.Application.Contracts.Services;
+
+public interface ITagService
+{
+    Task<TagDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    
+    Task<IEnumerable<TagDto>> GetPagedAsync(TagFilterDto tagFilterDto, CancellationToken cancellationToken);
+    
+    Task<IEnumerable<QuestionDto>> GetQuestionsByTagIdAsync(Guid id, CancellationToken cancellationToken);
+    
+    Task<TagDto> CreateAsync(CreateTagDto createTagDto, CancellationToken cancellationToken);
+    
+    Task UpdateAsync(UpdateTagDto updateTagDto, CancellationToken cancellationToken);
+    
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
