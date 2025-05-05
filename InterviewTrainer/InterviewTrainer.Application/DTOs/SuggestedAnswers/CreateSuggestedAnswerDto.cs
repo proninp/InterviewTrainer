5072a05cb7e39e5d3ada6d3d13069d5372f1ca11
proninp <proninpaul@yandex.ru>
2025-05-05 11:16:23 +0300

    Added SuggestedAnswerService

--- InterviewTrainer/InterviewTrainer.Application/DTOs/SuggestedAnswers/CreateSuggestedAnswerDto.cs
+++ InterviewTrainer/InterviewTrainer.Application/DTOs/SuggestedAnswers/CreateSuggestedAnswerDto.cs
@@ -1,3 +1,11 @@
-﻿namespace InterviewTrainer.Application.DTOs.SuggestedAnswers;
+﻿using InterviewTrainer.Domain.Entities;
+
+namespace InterviewTrainer.Application.DTOs.SuggestedAnswers;
 
 public record CreateSuggestedAnswerDto(Guid QuestionId, string Answer);
+
+public static class CreateSuggestedAnswerDtoExtension
+{
+    public static SuggestedAnswer ToSuggestedAnswer(this CreateSuggestedAnswerDto createSuggestedAnswerDto) =>
+        new(createSuggestedAnswerDto.QuestionId, createSuggestedAnswerDto.Answer);
+}
