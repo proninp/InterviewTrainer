1bbe9de5c46933d48fd88be769e2607030b1eb8d
proninp <proninpaul@yandex.ru>
2025-05-10 12:25:43 +0300

    Added FluentResult and chanhed UserService to ResultsPattern

--- InterviewTrainer/InterviewTrainer.Application.Abstractions/Services/IUserService.cs
+++ InterviewTrainer/InterviewTrainer.Application.Abstractions/Services/IUserService.cs
@@ -1,16 +1,17 @@
 ﻿using InterviewTrainer.Application.Contracts.Users;
+using FluentResults;
 
 namespace InterviewTrainer.Application.Abstractions.Services;
 
 public interface IUserService
 {
-    Task<UserDto> GetByIdAsync(long id, CancellationToken cancellationToken);
+    Task<Result<UserDto>> GetByIdAsync(long id, CancellationToken cancellationToken);
     
     Task<List<UserDto>> GetPagedAsync(UserFilterDto userFilterDto, CancellationToken cancellationToken);
     
-    Task<UserDto> CreateAsync(CreateUserDto createUserDto, CancellationToken cancellationToken);
+    Task<Result<UserDto>> CreateAsync(CreateUserDto createUserDto, CancellationToken cancellationToken);
     
-    Task UpdateAsync(UpdateUserDto updateUserDto, CancellationToken cancellationToken);
+    Task<Result> UpdateAsync(UpdateUserDto updateUserDto, CancellationToken cancellationToken);
     
     Task DeleteAsync(long id, CancellationToken cancellationToken);
 }
