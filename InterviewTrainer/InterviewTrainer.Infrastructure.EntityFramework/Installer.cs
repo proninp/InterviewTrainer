6753c426d521adfe2f45f75fc1e5f2ee31e2764a
proninp <proninpaul@yandex.ru>
2025-05-20 22:07:29 +0300

    Added DI services Installers

---
+++ InterviewTrainer/InterviewTrainer.Infrastructure.EntityFramework/Installer.cs
@@ -0,0 +1,16 @@
+﻿using InterviewTrainer.Application.Abstractions.Repositories;
+using InterviewTrainer.Infrastructure.EntityFramework.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InterviewTrainer.Infrastructure.EntityFramework;
+
+public static class Installer
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DbSettings>(configuration.GetSection(nameof(DbSettings))); 
+        services.AddDbContext<IUnitOfWork, DatabaseContext>();
+        return services;
+    }
+}
